b8a144d74772b5047df1d24eb9fdbf0c17a42614
RikuYokoo <s19C1123JF@s.chibakoudai.jp>
2023-12-12 09:48:18 +0900

    修正

--- ros2_ws/src/omni_wheel/src/test/omni_wheel_control_test.cpp
+++ ros2_ws/src/omni_wheel/src/test/omni_wheel_control_test.cpp
@@ -1,6 +1,10 @@
 
 #include "omni_wheel/omni_wheel_control.hpp"
 
+#include <iostream>
+#include <vector>
+#include <array>
+
 
 int main(int argc, char* argv[])
 {
@@ -11,17 +15,19 @@ int main(int argc, char* argv[])
     auto [motor1_ratio, motor2_ratio, motor3_ratio] = omni_wheel_control.moveRobot(Angle{AngleType::Degree, i}, 255);
     //omni_wheel_control.moveRobot(Angle{AngleType::Degree, static_cast<double>(i)}, 255).printAll();
 
+    /*
     std::array<double, 3> motor_ratio{motor1_ratio, motor2_ratio, motor3_ratio};
     std::vector<MotorCommand> motor_command;
     for(const auto &tmp_motor_ratio: motor_ratio)
     {
-      RotationDirection rotation_direction = RotationDirection::CW;
+      RotationDirection rotation_direction = RotationalDirection::CW;
       if(tmp_motor_ratio < 0)
       {
         rotation_direction = RotationDirection::CCW;
       }
       motor_command.emplace_back(MotorCommand{std::abs(tmp_motor_ratio), rotation_direction});
     }
+    */
   }
   //auto [] = omni_wheel_control();
 
