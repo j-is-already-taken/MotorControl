fe6651aea25a20e69adba610c919b7e91d5bbce1
RikuYokoo <s19C1123JF@s.chibakoudai.jp>
2023-12-17 10:02:35 +0900

    servo_controlのactionのclientを追加

---
+++ ros2_ws/src/servo_control/include/servo_control/servo_control_client.hpp
@@ -0,0 +1,44 @@
+#ifndef _SERVO_CONTROL_CLIENT_
+#define _SERVO_CONTROL_CLIENT_
+
+#include <functional>
+#include <future>
+#include <memory>
+#include <string>
+#include <sstream>
+
+
+#include "servo_control_interfaces/action/servo_control.hpp"
+
+#include "rclcpp/rclcpp.hpp"
+#include "rclcpp_action/rclcpp_action.hpp"
+#include "rclcpp_components/register_node_macro.hpp"
+
+namespace servo_control_client
+{
+  class ServoControlActionClient : public rclcpp::Node
+  {
+    public:
+      using ServoControl = servo_control_interfaces::action::ServoControl;
+      using GoalHandleServo = rclcpp_action::ServerGoalHandle<ServoControl>;
+
+      explicit ServoControlActionClient(const rclcpp::NodeOptions & options);
+
+      void send_goal();
+
+    private:
+      rclcpp_action::Client<ServoControl>::SharedPtr servo_control_client_ptr_;
+      rclcpp::TimerBase::SharedPtr timer_;
+
+      void goal_response_callback(const GoalHandleServo::SharedPtr & goal_handle);
+
+      void feedback_callback(
+          GoalHandleServo::SharedPtr,
+          const std::shared_ptr<const ServoControl::Feedback> feedback);
+
+      void result_callback(const GoalHandleServo::WrappedResult & result);
+  };  // class ServoControlActionClient
+
+}  // namespace action_tutorials_cpp
+
+#endif
---
+++ ros2_ws/src/servo_control/src/servo_control_client.cpp
@@ -0,0 +1,88 @@
+#include "servo_control/servo_control_client.hpp"
+
+namespace servo_control_client
+{
+  ServoControlActionClient::ServoControlActionClient(const rclcpp::NodeOptions & options)
+    : Node("servo_control_client", options)
+  {
+    this->client_ptr_ = rclcpp_action::create_client<ServoControl>(
+        this,
+        "servo_control");
+
+    /*
+    this->timer_ = this->create_wall_timer(
+        std::chrono::milliseconds(500),
+        std::bind(&ServoControlActionClient::send_goal, this));
+    */
+  }
+
+  void ServoControlActionClient::send_goal()
+  {
+    //using namespace std::placeholders;
+
+    //this->timer_->cancel();
+
+    if (!this->client_ptr_->wait_for_action_server()) {
+      RCLCPP_ERROR(this->get_logger(), "Action server not available after waiting");
+      rclcpp::shutdown();
+    }
+
+    auto goal_msg = ServoControl::Goal();
+    goal_msg.is_move = true;
+    //goal_msg.order = 10;
+
+    RCLCPP_INFO(this->get_logger(), "Sending goal");
+
+    auto send_goal_options = rclcpp_action::Client<ServoControl>::SendGoalOptions();
+    send_goal_options.goal_response_callback =
+      std::bind(&ServoControlActionClient::goal_response_callback, this, std::placeholders::_1);
+    send_goal_options.feedback_callback =
+      std::bind(&ServoControlActionClient::feedback_callback, this, std::placeholders::_1, std::placeholders::_2);
+    send_goal_options.result_callback =
+      std::bind(&ServoControlActionClient::result_callback, this, std::placeholders::_1);
+    this->client_ptr_->async_send_goal(goal_msg, send_goal_options);
+  }
+
+
+  void ServoControlActionClient::goal_response_callback(const GoalHandleServo::SharedPtr & goal_handle)
+  {
+    if (!goal_handle) {
+      RCLCPP_ERROR(this->get_logger(), "Goal was rejected by server");
+    } else {
+      RCLCPP_INFO(this->get_logger(), "Goal accepted by server, waiting for result");
+    }
+  }
+
+  void ServoControlActionClient::feedback_callback(
+      GoalHandleServo::SharedPtr,
+      const std::shared_ptr<const ServoControl::Feedback> feedback)
+  {
+    std::stringstream ss;
+    ss << "Next number in sequence received: ";
+    RCLCPP_INFO(this->get_logger(), ss.str().c_str());
+  }
+
+  void ServoControlActionClient::result_callback(const GoalHandleServo::WrappedResult & result)
+  {
+    switch (result.code) {
+    case rclcpp_action::ResultCode::SUCCEEDED:
+      break;
+    case rclcpp_action::ResultCode::ABORTED:
+      RCLCPP_ERROR(this->get_logger(), "Goal was aborted");
+      return;
+    case rclcpp_action::ResultCode::CANCELED:
+      RCLCPP_ERROR(this->get_logger(), "Goal was canceled");
+      return;
+    default:
+      RCLCPP_ERROR(this->get_logger(), "Unknown result code");
+      return;
+    }
+    std::stringstream ss;
+    ss << "Result received: ";
+    RCLCPP_INFO(this->get_logger(), ss.str().c_str());
+    rclcpp::shutdown();
+  }
+
+}  // namespace action_tutorials_cpp
+
+RCLCPP_COMPONENTS_REGISTER_NODE(servo_control_client::ServoControlActionClient)
