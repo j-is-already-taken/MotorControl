ce9de12729242660ee57c52e3d6177b9ecc45580
Dansato1203 <s19C1054bu@s.chibakoudai.jp>
2023-12-18 11:23:03 +0900

    Implement turning motion functionality

--- ros2_ws/src/omni_wheel/src/omni_wheel_client.cpp
+++ ros2_ws/src/omni_wheel/src/omni_wheel_client.cpp
@@ -2,6 +2,7 @@
 #include <future>
 #include <memory>
 #include <string>
+#include <chrono>
 
 #include "geometry_msgs/msg/point.hpp"
 #include "omni_wheel_interfaces/action/omni_wheel_control.hpp"
@@ -27,10 +28,20 @@ public:
     this->subscription_ = this->create_subscription<geometry_msgs::msg::Point>(
       "/center_coordinate", 5, std::bind(&OmniWheelActionClient::topic_callback, this, std::placeholders::_1));
 
+    this->timer_ = this->create_wall_timer(
+      std::chrono::milliseconds500(),
+      std::bind(&OmniWheelActionClient::timer_callback, this));
   }
 
   void topic_callback(const geometry_msgs::msg::Point::SharedPtr msg)
   {
+    last_received_ = this->now();
+    
+    if (is_turning_) {
+      this->client_ptr_->async_cacel_goal(current_goal_);
+      is_turning_ = false;
+	}
+
     double x = msg->x;
     double y = msg->y;
 
@@ -43,10 +54,19 @@ public:
     double angle_deg = angle_rad * (180.0 / M_PI);
 
     RCLCPP_INFO(this->get_logger(), "Object angle: %f degrees", angle_deg);
-    send_goal(angle_deg);
+    send_goal(angle_deg, turning=false);
    }
 
-  void send_goal(double angle_deg)
+  void timer_callback()
+  {
+    if ((this->now() - last_received_).seconds() >= 0.5 && !is_turning_) {
+      // If no message received for 0.5 seconds and not already turning
+      send__goal(angle_deg=1, turnig=true);
+      is_turning_ = true;
+    }
+  }
+
+  void send_goal(double angle_deg, bool turning)
   {
     using namespace std::placeholders;
 
@@ -58,7 +78,7 @@ public:
     auto goal_msg = OmniWheelControl::Goal();
     goal_msg.target_angle = angle_deg;
     goal_msg.move_millimeter = 5; // 仮の値
-    goal_msg.is_turning = false; // 仮の値
+    goal_msg.is_turning = turning;
 
     RCLCPP_INFO(this->get_logger(), "Sending goal");
 
@@ -69,13 +89,16 @@ public:
       std::bind(&OmniWheelActionClient::feedback_callback, this, _1, _2);
     send_goal_options.result_callback =
       std::bind(&OmniWheelActionClient::result_callback, this, _1);
-    this->client_ptr_->async_send_goal(goal_msg, send_goal_options);
+    current_goal_ = this->client_ptr_->async_send_goal(goal_msg, send_goal_options);
   }
 
 private:
   rclcpp_action::Client<OmniWheelControl>::SharedPtr client_ptr_;
   rclcpp::Subscription<geometry_msgs::msg::Point>::SharedPtr subscription_;
   rclcpp::TimerBase::SharedPtr timer_;
+  rclcpp::Time last_received_;
+  bool is_turning_;
+  GoalHandleOmniWheel::SharedPtr current_goal_;
 
   void goal_response_callback(const GoalHandleOmniWheel::SharedPtr & goal_handle)
   {
